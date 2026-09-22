89d2d288431d546094f94a919fe2ac4588b3f1ce
Abolfazl Motallebzadeh <abolfazlmotallebzadehcs@gmail.com>
2026-09-22 15:26:38 +0330

    the end of resume part

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,20 @@
 const navToggleIcon = document.querySelector('.nav__toggle-icon');
 const menu = document.querySelector(".menu");
 const cover = document.querySelector('.cover');
-
+const resumeListItems = document.querySelectorAll(".resume-list__item")
 
 navToggleIcon.addEventListener('click', ()=>{
     navToggleIcon.classList.toggle('nav__toggle-icon--open');
     menu.classList.toggle('menu--open');
     cover.classList.toggle('cover--show')
+});
+
+resumeListItems.forEach(resumeListItem => {
+    resumeListItem.addEventListener('click',()=>{
+        document.querySelector('.resume-list__item--active').classList.remove('resume-list__item--active');
+        resumeListItem.classList.add('resume-list__item--active');
+        document.querySelector('.resume-content--show').classList.remove('resume-content--show');
+        let ContentId = resumeListItem.getAttribute('data-content-id');
+        document.querySelector(ContentId).classList.add('resume-content--show');
+    })
 });
